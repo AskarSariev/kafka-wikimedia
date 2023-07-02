98778b244676034c9ce50fc6c864094444e5c7be
Askar Sariev <askar-64@mail.ru>
2023-07-02 20:10:53 +0300

    Create project

---
+++ kafka-consumer-database/src/main/java/com/example/KafkaDatabaseConsumer.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import com.example.entity.WikimediaData;
+import com.example.repository.WikimediaDataRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaDatabaseConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
+
+    private final WikimediaDataRepository wikimediaDataRepository;
+
+    @Autowired
+    public KafkaDatabaseConsumer(WikimediaDataRepository wikimediaDataRepository) {
+        this.wikimediaDataRepository = wikimediaDataRepository;
+    }
+
+    @KafkaListener(topics = "wikimedia_recentchange", groupId = "myGroup")
+    public void consume(String eventMessage) {
+        LOGGER.info(String.format("Event message recieved -> %s", eventMessage));
+        WikimediaData wikimediaData = new WikimediaData();
+        wikimediaData.setWikiEventData(eventMessage);
+        wikimediaDataRepository.save(wikimediaData);
+    }
+}
---
+++ kafka-consumer-database/src/main/java/com/example/repository/WikimediaDataRepository.java
@@ -0,0 +1,9 @@
+package com.example.repository;
+
+import com.example.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+}
